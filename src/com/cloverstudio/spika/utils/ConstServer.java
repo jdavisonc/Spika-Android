ffe2616f3dcba3945dfdc9131860d19cde3b43fd
jdavisonc <jdavisonc@gmail.com>
2014-04-22 00:58:29 -0300

    Override props with server

--- src/com/cloverstudio/spika/utils/ConstServer.java
+++ src/com/cloverstudio/spika/utils/ConstServer.java
@@ -3,8 +3,8 @@ package com.cloverstudio.spika.utils;
 public class ConstServer {
 
 	/* Change to your server adress */
-	public static final String BASE_URL                	= "http://officialapi.spikaapp.com";
-	public static final String INFORMATION_URL			= "http://officialapi.spikaapp.com/page/information/";
+	public static final String BASE_URL					= "http://54.187.91.208/spika-server/wwwroot";
+	public static final String INFORMATION_URL			= "http://54.187.91.208/spika-server/wwwroot/page/information/";
 	public static final String LIST_SERVERS_URL         = BASE_URL + "/" + Const.API_FOLDER + "servers";
 	
 }
